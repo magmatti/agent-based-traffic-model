eb7e4e0b8e3f020a9e0c6603497e5decc26f8788
Mateusz Wójtowicz <matiwojt@gmail.com>
2025-10-30 14:35:59 +0100

    add demo

--- experiments/run_cpu.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-python -m venv .venv && source .venv/bin/activate
-pip install -r requirements.txt
-python src/main.py --minutes ${1:-2} --arrival ${2:-600} --cycle ${3:-60}
